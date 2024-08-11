1ad7b0ff1bb7b042a500266b82e60a40e6ccea82
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-08-11 09:45:24 +0200

    implemento i component About e Contact (contact statico)

--- src/views/AppAbout.vue
+++ src/views/AppAbout.vue
@@ -1,9 +1,9 @@
-<script >
+<script>
 export default {
-    name: 'AppAbout',
- data() {
+  name: 'AppAbout',
+  data() {
     return {
-       
+
     }
   }
 }
@@ -11,10 +11,61 @@ export default {
 </script>
 
 <template>
-  <h5>About me...</h5>
-    
+  <div class="card-container">
+    <div class="card">
+      <h5>Welcome to [Your Company Name]</h5>
+      <p>At [Your Company Name], we're passionate about bringing people and entertainment together. Our team of
+      dedicated professionals is committed to providing a unique and engaging experience for film enthusiasts of all
+      ages.</p>
+      <h5>Our Story</h5>
+      <p>Founded in [Year], our company has grown from a small startup to a leading platform for film discovery and
+        exploration. Our journey began with a simple idea: to make it easy for people to find and enjoy great movies.
+        Today, we're proud to have built a community of millions of users who share our passion for cinema.</p>
+      <h5>Our Mission</h5>
+      <p>Our mission is to inspire a love of film in everyone. We believe that movies have the power to transport us to
+        new worlds, evoke strong emotions, and bring people together like nothing else can. That's why we're dedicated
+        to
+        providing a platform that's easy to use, informative, and fun.</p>
+      <h5>Our Values</h5>
+      <ul>
+        <li><b>Quality:</b> We're committed to</li>
+        <li><b>Innovation:</b> We're</li>
+        <li><b>Passion:</b> We're passionate</li>
+        <li><b>Community</b>We believe that film is a shared</li>
+      </ul>
+      <h5>Meet Our Team</h5>
+
+      <p>Our team is made up of film lovers, tech enthusiasts, and creative thinkers who are dedicated to making [Your
+        Company Name] the best it can be. From our developers to our content creators, every member of our team is
+        passionate about delivering an exceptional experience for our users.
+      </p>
+      <h5>Get in Touch
+      </h5>
+      <p>Thanks for stopping by! If you have any questions, comments, or just want to say hello, we'd love to hear from
+        you. Contact us at [Your Contact Information] or follow us on social media to stay up-to-date on the latest news
+        and updates from [Your Company Name].
+
+        Feel free to customize this text to fit your company's unique voice and style!</p>
+
+    </div>
+  </div>
+
 </template>
 
 <style scoped>
+.card-container {
+  background-color: #222222;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
+}
+
+.card {
+  background-color: rgba(221, 160, 221, 0.229);
+  padding: 2rem;
+  width: 80rem;
+  color: antiquewhite;
+}
 </style>
--- src/views/AppContact.vue
+++ src/views/AppContact.vue
@@ -11,10 +11,74 @@ export default {
 </script>
 
 <template>
+    <div class="card-container">
+        <div class="card mt-5">
+        <div class="col-12 p-4">
+            <h2>Contact me</h2>
+            <p>It’s important to note that the contact page is not the only place where you
+                should include contact information. You should also include a contact page on your website’s footer, as
+                well as on your About page. This way, visitors can easily find your contact information no matter
+                where they are on your site.
+            </p>
+            <template v-if="success">
+                <div class="alert alert-success" role="alert">
+                    <b>Messaggio inviato</b>
+                </div>
+            </template>
 
-    <h5>Contacts</h5>
+        </div>
 
+        <div class="col-12 p-4">
+            <form >
+                <div class="mb-3">
+                    <label for="name" class="form-label">Name</label>
+                    <input type="text" class="form-control" name="name" id="name" aria-describedby="helpId"
+                        placeholder="" v-model="name">
+                    <small id="nameHelper" class="form-text text-white">Write your name</small>
+                </div>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="text" class="form-control"name="email" id="email" aria-describedby="emailHelp"
+                        placeholder="" v-model="email">
+
+
+                    <small id="emailHelper" class="form-text text-white">Write your email</small>
+                </div>
+                <div class="mb-3">
+                    <label for="message" class="form-label">Message</label>
+                    <textarea type="text" class="form-control"name="message" id="message" rows="3" placeholder=""
+                        v-model="message">
+                    </textarea>
+
+                    <small id="messageHelper" class="form-text text-white">Write your email text</small>
+                </div>
+                <button type="submit" class="btn btn-primary">
+                    SEND
+                </button>
+            </form>
+        </div>
+
+    </div>
+    </div>
 
 </template>
 
-<style scoped></style>
+<style scoped>
+.card-container {
+    background-color: #222222;
+    padding: 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.card {
+    width: 60rem;
+    margin: 0.5rem;
+    /* height: 25rem; */
+    background-color: rgba(221, 160, 221, 0.229);
+    color: antiquewhite;
+}
+
+
+</style>
